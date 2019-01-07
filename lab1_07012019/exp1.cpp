cfd511a20b00beffc44c0919643506d11eb030a0
joshua dsouza <17co08@aitdgoa.edu.in>
2019-01-07 12:00:59 +0530

    chage

--- lab1_07012019/exp1.cpp
+++ lab1_07012019/exp1.cpp
@@ -4,44 +4,7 @@ void dectobin(int);
 int power(int ,int);
 void bintodec(int );
 
-
-	void bintodec (int x)
-	{
-		int a,n,res;
-		a=x;
-		n=x;
-		int count=0;
-		while(a!=0)
-		{a=a/10;
-			count++;
-		}
-		while(n!=0)
-		{
-			n=n%10;
-			for(int i=0;i<=count;i++)
-			{
-				res=n*power(2,i);
-			}
-		}
-		cout<<"result"<<res;
-	}
-	int main()
-{
-	cout<<"Enter chice";
-	cout<<"1.deecimal to binary "<<endl<<"2.binary to decimal";
-	int n,x;
-	cin>>n;
-	cout<<"enter the number";
-	cin>>x;
-	if(n==1)
-	{ dectobin(x);
-    }
-	else
-	{ bintodec(x);
-	}
-    return 0;
-}
-	void dectobin(int x)
+void dectobin(int x)
 	{  int a,res;
 		a=x;
 		res=0;
@@ -62,10 +25,55 @@ void bintodec(int );
 		}
 		else{
 		int po=1;
-		for(int i=y;i>0;i++)
+		for(int i=y;i>0;i--)
 		{
-			po=x*y;
+			po=po*x;
 		}
 		return po;
 	}
 }
+
+	void bintodec (int x)
+	{
+		int a,p,n,res=0;
+		a=x;
+		n=x;
+		p=x;
+		int count=0;
+		while(a!=0)
+		{a=a/10;
+			count++;
+		}
+		while(n!=0)
+		{
+			p=n%10;
+			for(int i=0;i<=count;i++)
+			{
+				res=res+p*power(2,i);
+			}
+			n=n/10;
+		}
+		cout<<"result"<<res;
+	}
+	int main()
+{
+
+	
+	int n,x;
+	
+	do {
+			cout<<"Enter chice";
+	cout<<"1.deecimal to binary "<<endl<<"2.binary to decimal";
+	cin>>n;
+	cout<<"enter the number";
+	cin>>x;
+		switch (n)
+	{case 1:dectobin(x);
+           break;
+	case 2:bintodec(x);
+	break;}
+}while (n!=0);
+	cout<<"thank you foir using this program";
+    return 0;
+}
+	
