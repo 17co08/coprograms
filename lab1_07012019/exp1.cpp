f738dd9a09c17602fe06bc3619091156d4426527
joshua dsouza <17co08@aitdgoa.edu.in>
2019-01-14 11:54:53 +0530

    final code without cout display

--- lab1_07012019/exp1.cpp
+++ lab1_07012019/exp1.cpp
@@ -1,79 +1,86 @@
 #include<iostream>
 using namespace std;
-void dectobin(int);
-int power(int ,int);
-void bintodec(int );
 
-void dectobin(int x)
-	{  int a,res;
-		a=x;
-		res=0;
-		while(a!=0)
-		{
-			a=a%2;
-			res=res*10+a;
-			a=a/10;
-		}
-				
-		cout<<res<<"is the binary equivalent of"<<x;
-	}
-	int power(int x,int y)
-	{ 
-		if(x==0)
-		{
-			return 0;
-		}
-		else{
-		int po=1;
-		for(int i=y;i>0;i--)
-		{
-			po=po*x;
-		}
-		return po;
-	}
-}
-
-	void bintodec (int x)
-	{
-		int a,p,n,res=0;
-		a=x;
-		n=x;
-		p=x;
-		int count=0;
-		while(a!=0)
-		{a=a/10;
-			count++;
-		}
-		while(n!=0)
-		{
-			p=n%10;
-			for(int i=0;i<=count;i++)
-			{
-				res=res+p*power(2,i);
-			}
-			n=n/10;
-		}
-		cout<<"result"<<res;
-	}
-	int main()
+int pow(int n, int m)
 {
+	int p=1;
+	
+	if(m==0)
+		return 1;
+	
+	for(int i=0;i<m;i++)
+		p=p*n;
+	
+	return p;
+	}
 
+int dec_2_bin(int dec)
+{
+	int bin,temp,rem,i;
 	
-	int n,x;
+	i=0;
+	bin=0;
+	temp=0;
 	
-	do {
-			cout<<"Enter chice";
-	cout<<"1.deecimal to binary "<<endl<<"2.binary to decimal";
-	cin>>n;
-	cout<<"enter the number";
-	cin>>x;
-		switch (n)
-	{case 1:dectobin(x);
-           break;
-	case 2:bintodec(x);
-	break;}
-}while (n!=0);
-	cout<<"thank you foir using this program";
-    return 0;
+	while(dec>0)
+	{   
+		i++;
+		rem=dec%2;
+		temp=temp*10+rem;
+		dec=dec/2;
+		}
+	
+	while(i>0)
+	{
+		bin=bin*10+temp%10;
+		temp=temp/10;
+		i--;
+		}
+	
+	return bin;
+	}
+
+int bin_2_dec(int bin)
+{
+	int rem,dec,p;
+	
+	p=0;
+	dec=0;
+	
+	while(bin>0)
+	{
+		rem=bin%10;
+		dec=dec+rem*pow(2,p);
+		p++;
+		bin=bin/10;
+		}
+	return dec;
+	}
+
+int main()
+{
+	int n,cnvrt,c;
+	do{
+		cout<<"Enter 1 to convert decimal to binary"<<endl;
+		cout<<"Enter 2 to convert binary to decimal"<<endl;
+		cout<<"Enter 0 to quit"<<endl<<endl; 
+		
+		cout<<"Enter choice:";
+		cin>>c;
+		cout<<"Enter number to be convertd:";
+		cin>>n;
+		
+		switch(c)
+		{
+			case 1: cnvrt=dec_2_bin(n);
+					cout<<"Binary equivalent is:"<<cnvrt<<endl;
+					break;
+			case 2: cnvrt=bin_2_dec(n);
+					cout<<"Decimal equivalent is:"<<cnvrt<<endl;
+					break;
+			}
+		}while(c!=0);
+	
+	
+	return 0;
 }
-	
